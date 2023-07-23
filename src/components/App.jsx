59a6c1099b1ca84fc795c0069543c8ce576e2ae4
Vladislav <80133226+Speerekk@users.noreply.github.com>
2023-07-23 14:33:13 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
+import React, { Suspense } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+const Home = React.lazy(() => import('./Home'));
+const Movies = React.lazy(() => import('./Movies'));
+const MovieDetails = React.lazy(() => import('./MovieDetails'));
+const Cast = React.lazy(() => import('./Cast'));
+const Reviews = React.lazy(() => import('./Reviews'));
+
+function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/movies" component={Movies} />
+          <Route exact path="/movies/:movieId" component={MovieDetails} />
+          <Route exact path="/movies/:movieId/cast" component={Cast} />
+          <Route exact path="/movies/:movieId/reviews" component={Reviews} />
+          <Redirect to="/" />
+        </Switch>
+      </Suspense>
+    </Router>
   );
-};
+}
+
+export default App;
