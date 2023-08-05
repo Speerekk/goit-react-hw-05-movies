3aaaed661adf4f37721a904246740a08435bba78
Vladislav <80133226+Speerekk@users.noreply.github.com>
2023-08-05 19:51:24 +0200

    fix

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,16 +1,45 @@
-// Movies.js
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-import MovieSearch from '../MovieSearch/MovieSearch';
+// components/Movies/Movies.js
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function Movies() {
-  const location = useLocation();
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = event => {
+    event.preventDefault();
+    axios
+      .get('https://api.themoviedb.org/3/search/movie', {
+        params: {
+          api_key: 'e9ca223ab9ca4a994e59de0722330ef2',
+          query: searchQuery,
+        },
+      })
+      .then(response => setSearchResults(response.data.results))
+      .catch(error => console.error(error));
+  };
 
   return (
     <div>
-      {location.pathname === '/movies' && <MovieSearch />}
-      <h2>Search Movies</h2>
-      {/* Add your movie search results here */}
+      <h2 className="search">Search Movies</h2>
+      <form onSubmit={handleSearch} className="search">
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
+          placeholder="Enter movie name"
+          className="search-in"
+        />
+        <button type="submit">Search</button>
+      </form>
+      <ul>
+        {searchResults.map(movie => (
+          <li key={movie.id}>
+            <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
